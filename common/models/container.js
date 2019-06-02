04c9e9d2cfdc28c5ccfa23985fc6fe78bd8b81cd
ak1103dev <ak.apichan1103@gmail.com>
2019-06-02 08:33:02 +0700

    :construction: can resize image

--- common/models/container.js
+++ common/models/container.js
@@ -1,12 +1,49 @@
 'use strict'
 
+const axios = require('axios')
+const sharp = require('sharp')
+
+const apiUrl = 'http://localhost:3000'
+
 module.exports = function(Container) {
   Container.preview = async function(container, file, req, res) {
+    const url = `${apiUrl}/api/Containers/${container}/download/${file}`
     try {
-      const data = await this.download(container, file, req, res)
+      const fileData = await axios.get(url, {
+        responseType: 'arraybuffer',
+      })
+        .then(response => new Buffer(response.data, 'binary'))
+      const data = await sharp(fileData)
+        .rotate()
+        .resize(200)
+        .toBuffer()
       return data
     } catch (e) {
-      return e
+      console.error(e)
+      return Promise.reject(e)
+    }
+  }
+
+  Container.cover = async function(container, file, req, res, width = 500, height = 500) {
+    const url = `${apiUrl}/api/Containers/${container}/download/${file}`
+    try {
+      const fileData = await axios.get(url, {
+        responseType: 'arraybuffer',
+      })
+        .then(response => new Buffer.from(response.data, 'binary'))
+      const data = await sharp(fileData)
+        .resize({
+          width,
+          height,
+          fit: sharp.fit.outside,
+        })
+        .jpeg()
+        .toBuffer()
+      res.contentType('image/jpeg')
+      return res.send(data)
+    } catch (e) {
+      console.error(e)
+      return Promise.reject(e)
     }
   }
 }
